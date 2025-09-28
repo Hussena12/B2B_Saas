87be59ff6cca1152301e6313e232a26b9aed9d4d
Hussen <ha0959943@gmail.com>
2025-09-28 10:06:00 +0300

    feat: convex package setup

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,20 +1,21 @@
-import { add } from "@workspace/math/add";
+"use client";
+
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
-import { Input } from "@workspace/ui/components/input";
 
 export default function Page() {
+  const users = useQuery(api.users.getmany);
+  const addUsers = useMutation(api.users.add);
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World/widget</h1>
-        <h1 className="text-2xl font-bold">Hello World/widget</h1>
-        <h1 className="text-2xl font-bold">Hello World/widget</h1>
-        <h1 className="text-2xl font-bold">Hello World/widget</h1>
-        <h1 className="text-2xl font-bold">Hello World/widget</h1>
-        <h1 className="text-2xl font-bold">Hello World/widget</h1>
-        <Button size="sm">Button</Button>
-        <Input />
-        {add(2, 3)}
+        <h1 className="text-2xl font-bold">apps/widget</h1>
+        <Button onClick={() => addUsers()}>add</Button>
+        <div className="max-w-sm w-full mx-auto">
+          {JSON.stringify(users, null, 2)}
+        </div>
       </div>
     </div>
   );
